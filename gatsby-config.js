76cce5a365446353f862d7076fc6d1cb3fbc2ab6
p0wer0ff <34498885+p0wer0ff@users.noreply.github.com>
2020-04-20 17:46:55 +0800

    Update gatsby-config.js
    再试试

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/younotion/174a275e5c6a48429e5eeba61aee6b0d?v=cdc0d7d4fdb84f11b07c4446e38b6d2e"
+                configTable: "https://www.notion.so/174a275e5c6a48429e5eeba61aee6b0d?v=cdc0d7d4fdb84f11b07c4446e38b6d2e"
             }
         },
         {     
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/younotion/c83cfdb141ce4a2395f06ed8662da428?v=83fe0ffd997e4b028182f0dbcf0f23a1"
+                configTable: "https://www.notion.so/c83cfdb141ce4a2395f06ed8662da428?v=83fe0ffd997e4b028182f0dbcf0f23a1"
             }
         }
     ],
